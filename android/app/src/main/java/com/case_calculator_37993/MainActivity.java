841c8352bc1dd0616a3aec0776929fb81cde968e
Crowdbotics <team@crowdbotics.com>
2022-12-23 00:05:56 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/case_calculator_37993/MainActivity.java
@@ -0,0 +1,15 @@
+package com.case_calculator_37993;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "case_calculator_37993";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "case_calculator_37993",
+  url: "https://case_calculator_37993.botics.co",
+  api: "https://case_calculator_37993.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
